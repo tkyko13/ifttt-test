331b0e9ae2e8bc8bff239d04217e1c22aefd40b0
tkyk <tkyk.o13@gmail.com>
2017-12-22 16:30:30 +0900

    getの取得

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var app = express();
 var PORT = process.env.PORT || 8080;
  
 app.get('/', function (req, res) {
-	res.send('Hello World');
+	if (req.query.myTest) {
+		res.send("get myTest = "+req.query.myTest);
+		console.log("get myTest = "+req.query.myTest);
+	}
 });
  
 app.listen(PORT);
