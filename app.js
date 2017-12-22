006ce8f972cb254b7335762af7acc42a650f7bd9
tkyk <tkyk.o13@gmail.com>
2017-12-22 13:59:25 +0900

    ポートをherokuなどに対応

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 8080;
  
 app.get('/', function (req, res) {
 	res.send('Hello World');
 });
  
-app.listen(3000);
+app.listen(PORT);
